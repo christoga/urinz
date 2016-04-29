a1cd4af1f48d3194b2f4ccf214d35b8a5caeffcd
Andre Christoga <andrechristoga@gmail.com>
2016-04-29 19:18:08 +0700

    Updated android platform

---
+++ platforms/android/assets/www/js/main.js
@@ -0,0 +1,20 @@
+function cute(brea) {
+  switch (brea) {
+    case 1:
+    case 2:
+    case 3:
+      alert('Anda terhidrasi dengan baik');
+      break;
+
+    case 4:
+    case 5:
+    case 6:
+      alert('Anda kurang terhidrasi dengan baik');
+      break;
+
+    case 7:
+    case 8:
+      alert('Anda mengalami kekurangan cairan');
+      break;
+  }
+}
