639542b44f967877e373b0851084b1829d8d48e5
Andre Christoga <andrechristoga@gmail.com>
2016-04-30 08:52:41 +0700

    Danny Drinkwater

--- www/js/main.js
+++ www/js/main.js
@@ -9,12 +9,12 @@ function cute(brea) {
     case 4:
     case 5:
     case 6:
-      alert('Kamu kurang terhidrasi dengan baik');
+      alert('Kamu kurang terhidrasi dengan baik \n Ayo minum 3-4 gelas lagi');
       break;
 
     case 7:
     case 8:
-      alert('Kamu mengalami kekurangan cairan');
+      alert('Kamu mengalami kekurangan cairan \n Ayo minum 5-6 gelas lagi');
       break;
   }
 }
