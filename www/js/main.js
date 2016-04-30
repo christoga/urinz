d5dae3235f543eac7b3fe8782d82309a4d1a9376
Andre Christoga <andrechristoga@gmail.com>
2016-04-30 08:46:21 +0700

    Remove kamu with anda

--- www/js/main.js
+++ www/js/main.js
@@ -3,18 +3,18 @@ function cute(brea) {
     case 1:
     case 2:
     case 3:
-      alert('Anda terhidrasi dengan baik');
+      alert('Kamu terhidrasi dengan baik');
       break;
 
     case 4:
     case 5:
     case 6:
-      alert('Anda kurang terhidrasi dengan baik');
+      alert('Kamu kurang terhidrasi dengan baik');
       break;
 
     case 7:
     case 8:
-      alert('Anda mengalami kekurangan cairan');
+      alert('Kamu mengalami kekurangan cairan');
       break;
   }
 }
