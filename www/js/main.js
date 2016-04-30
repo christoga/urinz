80b727a17678696324858bfead378d313cbe3742
Andre Christoga <andrechristoga@gmail.com>
2016-04-30 09:31:11 +0700

    Can't Help Falling In Love with U

--- www/js/main.js
+++ www/js/main.js
@@ -9,12 +9,12 @@ function cute(brea) {
     case 4:
     case 5:
     case 6:
-      alert('Kamu kurang terhidrasi dengan baik \n Ayo minum 3-4 gelas lagi');
+      alert('Kamu kurang terhidrasi dengan baik \nAyo minum 3-4 gelas lagi');
       break;
 
     case 7:
     case 8:
-      alert('Kamu mengalami kekurangan cairan \n Ayo minum 5-6 gelas lagi');
+      alert('Kamu mengalami kekurangan cairan \nAyo minum 5-6 gelas lagi');
       break;
   }
 }
